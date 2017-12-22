1238c6cfd89d827d88f1c309d2e9a604248da207
rjarek <softart.poczta@gmail.com>
2017-12-22 18:42:25 +0100

    start-dziala

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        //return 
+        
+        $user_id = auth()->user()->id; 
+
+        $user = User::find($user_id);
+
+        //$user->listings;
+
+        return view('dashboard')->with('listings', $user->listings);
+    }
+}
